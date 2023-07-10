71559151e7aab5b406ba19d3621f960375df653d
Andreas Rozek <A.Rozek@gmx.de>
2023-07-10 18:01:11 +0200

    Update HTMLView.js

--- basicControls/HTMLView.js
+++ basicControls/HTMLView.js
@@ -1,15 +1,35 @@
-  const { expectText } = SPW
+  const { expectText, expectTextline } = SPW
 
   export function initialize (
     Work,Sheet,Patch, installStyle, reactively,
     $,define$Property, fill$fromJSON,JSONfor$, html
   ) {
+    installStyle(`
+      .SPW_HTMLView {
+        display:block; position:absolute;
+        left:0px; top:0px; right:0px; bottom:0px;
+      }
+    `)
+
+    $._ClassList = ''
+    $._Value     = ''
+
+    define$Property(
+      'ClassList',
+      function () { return $._ClassList },
+      function (newValue) {
+        expectTextline('CSS class list',newValue)
+          newValue = newValue.trim().replace(/s+/g,' ')
+        if ($._ClassList !== newValue) { $._ClassList = newValue }
+      }
+    )
+
     define$Property(
       'Value',
       function () { return $._Value },
       function (newValue) {
         expectText('HTMLView value',newValue)
-        $._Value = newValue
+        if ($._Value !== newValue) { $._Value = newValue }
       }
     )
 
@@ -20,7 +40,7 @@
     }
 
     reactively(() => {
-      this.Render = html`<div dangerouslySetInnerHTML=${{ __html:$._Value }}></div>`
+      this.Render = html`<div class="SPW_HTMLView ${$._ClassList}"
+        dangerouslySetInnerHTML=${{ __html:$._Value }}></div>`
     })
   }
-
